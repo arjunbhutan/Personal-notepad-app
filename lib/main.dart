acdaffc201c630997ce961e29f50884ae29acd02
Arjun Mongar <arjunmongar45@gmail.com>
2024-06-26 13:45:41 +0530

    changed wordings

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(title: 'Kuzu Zangpo la'),
+      home: const MyHomePage(title: 'Kuzu Zangpo'),
     );
   }
 }
